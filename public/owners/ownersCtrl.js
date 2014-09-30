abab11c45644b338487ef6d8512d4b5b1f0b8348
Andrew Levy <levyandrew44@yahoo.com>
2014-09-30 16:18:40 -0400

    error fixing

--- public/owners/ownersCtrl.js
+++ public/owners/ownersCtrl.js
@@ -3,7 +3,7 @@
 
     angular
         .module('owners')
-        .controller('ownersCtrl', ['$scope', 'ownersSvc', '$location', '$routeParams', '$rootScope', function ($scope, ownersSvc, $location, $routeParams,) {
+        .controller('ownersCtrl', ['$scope', 'ownersSvc', '$location', '$routeParams', '$rootScope', function ($scope, ownersSvc, $location, $routeParams) {
             ownersSvc.getOwners().success(function (owners) {
                 $scope.owners = owners;
             });
