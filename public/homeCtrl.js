8c09b4441617c118ab1c818b7540a66ccdb4215f
Andrew Levy <levyandrew44@yahoo.com>
2014-10-01 15:28:44 -0400

    fixing login

--- public/homeCtrl.js
+++ public/homeCtrl.js
@@ -4,7 +4,7 @@
     angular
         .module('bark.it')
         .controller('homeCtrl',['$scope', '$cookieStore', '_', 'ownersSvc', '$location', function ($scope, $cookieStore, _, ownersSvc, $location) {
-          $scope.login = function () {
+          $scope.login = function (email) {
            ownersSvc.getOwners().success(function (users) {
             var currentUser = _.find(users, {email : email});
             $cookieStore.put("currentuser", currentUser)
