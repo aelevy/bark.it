ff1dc089b253beddb16fb0d22bb9f3582debaa41
Andrew Levy <levyandrew44@yahoo.com>
2014-10-01 15:04:23 -0400

    adding login

--- public/homeCtrl.js
+++ public/homeCtrl.js
@@ -3,8 +3,14 @@
 
     angular
         .module('bark.it')
-        .controller('homeCtrl',['$scope', function ($scope) {
-
+        .controller('homeCtrl',['$scope', function ($scope, $cookieStore, _, ownersSvc, $location) {
+          $scope.login = function () {
+           ownersSvc.getOwners().success(function () {
+            var currentUser = _.find(users, {email : email});
+            $cookieStore.put("currentuser", currentUser)
+            $location.path("/owners" + currentUser_.id)
+           })
+          }
         }]);
 
 })();
